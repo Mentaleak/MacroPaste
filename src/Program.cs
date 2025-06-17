a9013e80b0eb9b4f0ae54dbd7ff946c80aafc3c9
Zachary Fischer <zfischer@dutchessny.gov>
2025-06-17 15:30:42 -0400

    fixed the multiple running instances issue

--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -10,12 +11,23 @@ namespace MacroCopyPaste
 {
     internal static class Program
     {
+        private static Mutex mutex;
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            bool isNewInstance;
+            mutex = new Mutex(true, "MacroCopyPasteApp", out isNewInstance);
+
+            if (!isNewInstance)
+            {
+                MessageBox.Show("Another instance of the application is already running.", "Instance Check", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new TrayAppContext());
